729bcbdaa3a70c31739b400a223bbee02deb6fbe
sabbirnewbd71-debug <sabbirnewbd71@gmail.com>
2025-11-10 23:09:36 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-
+<script> const shareButton = document.querySelector('.share-button'); if (navigator.share) { shareButton.addEventListener('click', (event) => { event.preventDefault(); navigator.share({ title: document.title, url: window.location.href }) .then(() => console.log('Successful share')) .catch((error) => console.log('Error sharing:', error)); }); } else { console.log('Web Share API not supported'); } </script>
